8ecb076ce2fc1d98220638cb374963e9725e6f12
chamsbejaoui <mohamedchamseddine.bejaoui@esprit.tn>
2025-04-11 11:36:30 +0100

    l'integration d'un calendrier vide

---
+++ src/Controller/CalendrierController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CalendrierController extends AbstractController
+{
+    #[Route('/calendrier', name: 'app_calendrier')]
+    public function index(): Response
+    {
+        return $this->render('calendrier/index.html.twig', [
+            'controller_name' => 'CalendrierController',
+        ]);
+    }
+}
